756482274c1e1fecb391b1ccf2a61e2593ea05cb
Joko Saputro <jokosaputro616@gmail.com>
2023-01-29 01:19:07 +0700

    setup environment, api albums, exceptions

---
+++ src/api/albums/handler.js
@@ -0,0 +1,94 @@
+const ClientError = require('../../exceptions/ClientError');
+
+class AlbumsHandler {
+    constructor(service, validator) {
+        this._service = service;
+        this._validator = validator;
+
+        this.postAlbumHandler = this.postAlbumHandler.bind(this);
+        this.getAlbumsHandler = this.getAlbumsHandler.bind(this);
+        this.getAlbumByIdHandler = this.getAlbumByIdHandler.bind(this);
+        this.putAlbumByIdHandller = this.putAlbumByIdHandller.bind(this);
+        this.deleteAlbumByIdHandler = this.deleteAlbumByIdHandler.bind(this);
+    }
+
+    async postAlbumHandler(request, h) {
+        try{
+            this._validator.validateAlbumPayload(request.payload);
+            
+            const { name, year } = request.payload;
+            const albumId = await this._service.addAlbum({ name, year });
+
+            const response = h.response({
+                status: 'success',
+                message: 'Album berhasil ditambahkan',
+                data: {
+                    albumId,
+                },
+            });
+            response.code(201);
+            return response
+        } catch (error) {
+            if (error instanceof ClientError) {
+                const response = h.response({
+                    status: 'fail',
+                    message: error.message
+                });
+                response.code(error.statusCode);
+                return response;
+            }
+
+            // Server Error!
+            const response = h.response({
+                status: 'error',
+                message: 'Maaf, terjadi kegagalan pada server kami.',
+            });
+            response.code(500);
+            console.log(error);
+            return response;
+        }
+    }
+
+    async getAlbumsHandler() {
+        const albums = await this._service.getAlbums();
+        return {
+            status: 'success',
+            data: {
+                albums,
+            },
+        };
+    }
+
+    async getAlbumByIdHandler(request, h) {
+        try {
+            const { id } = request.params;
+            const album = await this._service.getAlbumById(id);
+            return {
+                status: 'success',
+                data: {
+                    album,
+                },
+            };
+        } catch (error) {
+            if (error instanceof ClientError) {
+                const response = h.response({
+                    status: 'fail',
+                    message: error.message,
+                });
+                response.code(error.statusCode);
+                return response;
+            }
+
+            // Server Error !
+            const response = h.response({
+                status: 'fail',
+                message: 'Maaf, terjadi kegagalan pada server kami.',
+            });
+            response.code(500);
+            console.log(error);
+            return response;
+        }
+    }
+}
+
+module.exports = AlbumsHandler;
